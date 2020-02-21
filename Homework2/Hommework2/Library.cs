8571f97d614fffe053a1d4c1a6ba9460d11c253f
aveliate <alastarix@outlook.com>
2020-02-22 01:49:44 +0300

    Homework 2

---
+++ Homework2/Hommework2/Library.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    /// <summary>
+    /// Управление графикой и навигацией консоли
+    /// </summary>
+    public static class Draw
+    {  
+        /// <summary>
+        /// Устанавливает базовые параметры для программы
+        /// </summary>
+        /// <param name="Title">Заголовок для окна консоли</param>
+        public static void StartUp(string Title)
+        {
+            Console.CursorVisible = false;
+            Console.Title = Title;
+            Console.SetBufferSize(Console.LargestWindowWidth - 20, Console.LargestWindowHeight - 20);
+            Console.SetWindowSize(Console.LargestWindowWidth - 20, Console.LargestWindowHeight - 20);
+        }
+
+        /// <summary>
+        /// Вызывает подобие диалогового окна для ввода
+        /// </summary>
+        /// <param name="Text">Заголовок окна</param>
+        /// <param name="Prompt">Строка запроса ввода</param>
+        /// <returns>Отдаёт строку</returns>
+        public static string DialogBox(string Text, string Prompt)
+        {
+            string onto = "  Введите ответ, и нажмите клавишу Enter.  ";
+            int menuWidth = 80;
+            string clean = new string(' ', menuWidth);
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition((Console.WindowWidth - menuWidth) / 2, 16);
+            Print(16, clean);
+            Print(18, clean);
+            Print(17, clean);
+            Print(16, Text);
+            Print(18, onto);
+            Console.SetCursorPosition((Console.WindowWidth - 3 - Prompt.Length) / 2, 17);
+            Console.Write(Prompt);
+            string reply = Console.ReadLine();
+            Console.ResetColor();
+            Console.Clear();
+            return reply;
+        }
+
+        
+        /// <summary>
+        /// Вывод строки по центру экрана
+        /// </summary>
+        /// <param name="Line">Строка для вывода. Если 0, просто печатаем дальше.</param>
+        /// <param name="Text">Текст для выведения.</param>
+        public static void Print(int Line, string Text)
+        {
+            if (Line == 0) //проверяем, дали ли нам указатель на линию. Если дали 0, просто спускаемся дальше.
+            {
+                Console.SetCursorPosition(((Console.WindowWidth - (Text.Length)) / 2), Console.CursorTop);
+            }
+            else
+            { Console.SetCursorPosition(((Console.WindowWidth - Text.Length) / 2), Line); }
+
+            Console.WriteLine(Text);
+        }
+
+        /// <summary>
+        /// Функция печати меню - в зависимости от функции, с указанием выбранного пункта.
+        /// </summary>
+        /// <param name="MenuType">Номер меню: определяет, какое меню выводить (см.описание).</param>
+        /// <param name="Choice">Переменная, указывающая активный пункт меню.</param>
+        /// <param name="Current">Номер пункта, печатаемого сейчас.</param>
+        /// <param name="Text">Текст для выведения.</param>
+        public static void MSelect(int MenuType, int Choice, int Current, string Text)
+        {
+            ConsoleColor Recall1 = Console.ForegroundColor;
+            ConsoleColor Recall2 = Console.BackgroundColor;
+
+            //MenuType
+            //1 - Главное меню. 2 - Настройки. 3 - Горизонтальное меню. 4 - Сортировка
+
+            switch (MenuType)
+            {
+                case 1:
+                    if (Choice != Current) { Print(0, Text); }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.BackgroundColor = ConsoleColor.DarkBlue;
+                        Print(0, Text);
+                        Console.ForegroundColor = Recall1;
+                        Console.BackgroundColor = Recall2;
+                    }
+                    break;
+                case 2:
+                    Console.SetCursorPosition(5, Console.CursorTop);
+                    if (Choice != Current) { Console.WriteLine(Text); }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.BackgroundColor = ConsoleColor.DarkBlue;
+                        Console.WriteLine(Text);
+                        Console.ForegroundColor = Recall1;
+                        Console.BackgroundColor = Recall2;
+                    }
+                    break;
+                case 3:
+                    Console.SetCursorPosition((2 + Current * 18), 20);
+                    if (Choice != Current) { Console.Write(Text, 30); }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.BackgroundColor = ConsoleColor.DarkBlue;
+                        Console.Write(Text, 30);
+                        Console.ForegroundColor = Recall1;
+                        Console.BackgroundColor = Recall2;
+                    }
+                    break;
+                case 4:
+                    Console.SetCursorPosition((38), 21+Current);
+                    if (Choice != Current) { Console.Write(Text, 20); }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.BackgroundColor = ConsoleColor.DarkBlue;
+                        Console.Write(Text, 20);
+                        Console.ForegroundColor = Recall1;
+                        Console.BackgroundColor = Recall2;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+
+        /// <summary>
+        /// Функция управления главным меню.
+        /// </summary>
+        /// <param name="Header">Массив строк - заголовков для вывода вверху страницы.</param>
+        /// <param name="Itemset">Перечень пунктов меню.</param>
+        public static int MainMenu(string[] Header, string[] Itemset)
+        {
+            int Choice = 0;
+
+            ConsoleColor defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Print(1, Header[0]);
+            Console.ForegroundColor = defaultColor;
+
+            for (int x = 1; x < (Header.Length); x++)
+            {
+                Print(1 + x, Header[x]);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            while (true)
+            {
+                Console.SetCursorPosition(1, 7);
+                for (int i = 0; i < (Itemset.Length); i++)
+                {
+                    MSelect(1, Choice, i, Itemset[i]);
+                }
+                Console.WriteLine("");
+                MSelect(1, Choice, Itemset.Length, "Выход");
+
+                ConsoleKeyInfo InKey = Console.ReadKey();
+
+                if (InKey.Key == ConsoleKey.UpArrow) { if (Choice > 0) { Choice--; } }     //Выбираем верхний вариант, если он есть.
+                if (InKey.Key == ConsoleKey.DownArrow) { if (Choice < Itemset.Length) { Choice++; } }   //Выбираем нижний вариант, если он есть.
+
+                if (InKey.Key == ConsoleKey.Enter) return Choice;
+                
+            }
+        }
+        
+    }
+
+    ///Класс точка с двумя координатами и функцией запроса
+    public class Dot
+    {
+        public int x;
+
+        public int y;
+
+        string number;
+
+        public Dot(int xIn, int yIn)
+        {
+            this.x = xIn;
+            this.y = yIn;
+        }
+
+        public Dot(string number)
+        {
+            string xIn, yIn;
+            this.number = number;
+
+            do
+            {
+                xIn = Draw.DialogBox("Введите координату X "+ number +" точки:", "");
+            } while (!Int32.TryParse(xIn, out this.x));
+
+            do
+            {
+                yIn = Draw.DialogBox("Введите координату Y " + number + " точки:", "");
+            } while (!Int32.TryParse(yIn, out this.y));
+        }
+
+    }
+
+
+
+}
+
+
+
---
+++ Homework2/Hommework2/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Переменные
+            string[] menuHeader = new string[] {"Домашнее задание 2. GeekBrains C#",
+                                    "Пожалуйста, выберите искомый пункт меню, используя стрелки, и подтвердите клавишей ВВОД." };
+            string[] menuList = new string[] {"Задание  1. Наименьшее и наибольшее из трёх чисел",
+                                    "Задание 2. Подсчёт количества цифр в числе", 
+                                    "Задание 6. Подсчёт числа 'хороших' чисел до 1 млрд",
+                                    "Задание 7. Рекурсивная функция"};
+            bool looper = false;
+            #endregion
+
+            Draw.StartUp("Домашнее задание №2. Дмитрий Политов");
+
+            for (int i = 0; i < 3 && !looper; i++) looper = Auth("root", "GeekBrains");
+
+            while (looper)
+            {
+                Console.Clear();
+                switch (Draw.MainMenu(menuHeader, menuList))
+                {
+                    case 0:
+                        Task1();
+                        break;
+                    case 1:
+                        Task2();
+                        break;
+                    case 2:
+                        Task6();
+                        break;
+                    case 3:
+                        Task7();
+                        break;
+                    default:
+                        looper = false;
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Юмористическая версия авторизации. ОСТОРОЖНО: ВЗЛОМАНА РУССКИМИ ХАКЕРАМИ!
+        /// </summary>
+        /// <param name="login"></param>
+        /// <param name="psw"></param>
+        /// <returns></returns>
+        public static bool Auth(string login, string psw)
+        {
+            Draw.Print(5, "Для продолжения работы введите логин и пароль...");
+            Draw.Print(Console.BufferHeight/2 - 5, "ХАКЕРЫ СОВЕТУЮТ: попробуйте написать "+login);
+            string inLogin = Draw.DialogBox("Введите ЛОГИН", "");
+            Draw.Print(Console.BufferHeight / 2 - 5, "ХАКЕРЫ СОВЕТУЮТ: попробуйте написать "+psw);
+            string inPsw = Draw.DialogBox("Введите ПАРОЛЬ","");
+            return (inLogin == login && inPsw == psw);
+        }
+
+        /// <summary>
+        /// Наименьшее и наибольшее из трёх чисел
+        /// </summary>
+        public static void Task1()
+        {
+            Console.Clear();
+            Draw.Print(2, "Задание 1. Наименьшее и наибольшее из трёх чисел");
+            Draw.Print(4, "Для вашего удобства, числы предоставлены системным временем компьютера.");
+            Random rnd = new Random();
+            int a = rnd.Next(0, 1001);
+            int b = rnd.Next(0, 1001);
+            int c = rnd.Next(0, 1001);
+
+            Console.WriteLine("\n\nНаши счастливчики это числа 'a' = "+ a +", 'b' = "+ b +" и 'c' = " + c);
+            Console.WriteLine("Разумеется, максимальное из них - " + (a>b && a>c?a:(b>a&&b>c?b:c)));
+            Console.WriteLine("А минимальное - " + (a < b && a < c ? a : (b < a && b < c ? b : c)));
+            Draw.Print(Console.BufferHeight - 2, "Нажмите любую клавишу для продолжения...");
+            Console.ReadKey();
+
+        }
+
+        /// <summary>
+        /// Сколько же цифр в этом числе?..
+        /// </summary>
+        public static void Task2()
+        {
+            Console.Clear();
+            Random rnd = new Random();
+            Draw.Print(2, "Задание 2. Сколько цифр в числе?");
+            Draw.Print(4, "Для вашего удобства, числа предоставлены системным временем компьютера.");
+            int a = rnd.Next();
+            int b = rnd.Next(); 
+            int c = rnd.Next();
+            Console.WriteLine("\n\n\nНа сцене счастливчики, три числа, в каждом из которых...");
+            Console.WriteLine("Число 'а', " + a + ", в котором " + a.ToString().Length + " символов");
+            Console.WriteLine("Число 'b', " + b + ", в котором " + b.ToString().Length + " символов");
+            Console.WriteLine("Число 'c', " + c + ", в котором " + c.ToString().Length + " символов");
+
+            Draw.Print(Console.BufferHeight - 2, "Нажмите любую клавишу для продолжения...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Задачка на сходить попить кофе и не перенапрягаться
+        /// </summary>
+        public static void Task6()
+        {
+            int counter = 0;
+            Console.Clear();
+            Draw.Print(2, "Задание 6.Подсчёт числа 'хороших' чисел до 1 млрд");
+            DateTime startTime = DateTime.Now;
+            Console.WriteLine("\n\nБезобразие начато в " + startTime.ToString("HH:mm:ss"));
+            Console.Write("                      ]\r" + "[");
+            for (int i = 1; i <=1_000_000_000;i++)
+            {
+                if (i%Crunch(i) == 0) counter++;
+                if (i % 50_000_000 == 0) Console.Write("*");
+            }
+            Console.Write("]");
+            string elapsed = DateTime.Now.Subtract(startTime).ToString();
+            Console.WriteLine("\n\n Закончено безобразие в " + DateTime.Now.ToString("HH:mm:ss"));
+            Console.WriteLine("\n За это время мы насчитали " + counter + " годных чисел.");
+            Console.Write("Времени потрачено " + elapsed);
+
+            Console.Write("\n\nДля продолжения нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+
+        public static int Crunch(Int64 victim)
+        {
+            int digitsLength = victim.ToString().Length;
+            int sum = 0;
+            for (int i = 0; i<digitsLength;i++)
+            {
+                sum += Convert.ToInt32(Char.GetNumericValue(victim.ToString()[i]));
+            }
+            return sum;
+        }
+
+        /// <summary>
+        /// Рекурсия, вызывающая рекурсию...
+        /// </summary>
+        public static void Task7()
+        {
+            Console.Clear();
+            Draw.Print(2, "Задание 7. Рекурсия про сумму и обратный отсчёт");
+            Draw.Print(4, "Для вашего удобства, числа предоставлены системным временем компьютера.");
+            Random rnd = new Random();
+            int a = 0, b = 0, i = 0;
+            Int64 sum = 0;
+            while (!(a<b)) { a = rnd.Next(1, 1025); b = rnd.Next(1, 1025); i++; }
+            Console.WriteLine("Нам удалось подобрать годную комбинацию с " + i + " попытки.");
+            Console.WriteLine("Это числа "+ a +" и "+ b + ".");
+            Console.WriteLine("Вот разделяющие их числа - ");
+            Task7Func(a, b, ref sum);
+            Console.WriteLine("\n\nА сумма их - " + sum);
+            Console.Write("\n\nДля продолжения нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+
+        public static void Task7Func(int start, int end, ref Int64 sum)
+        {
+            Console.Write(" " + start);
+            if (start < end)
+            { sum += start; Task7Func(++start, end, ref sum); }
+        }
+
+
+    }
+}
